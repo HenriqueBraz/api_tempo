776722d7a416ebc46eb5c86a896199cb2f65728e
Henrique <henrique.braz@acad.pucrs.br>
2020-05-25 01:56:46 -0300

    make it better

--- api/views/app.py
+++ api/views/app.py
@@ -1,7 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for
+from werkzeug.utils import redirect
+
 from api.services. api_tempo import apiTempo
 
 app = Flask(__name__)
+
+@app.route("/favicon.ico")
+def favicon():
+    return redirect(url_for('index', flag=1))
+
+@app.route('/', methods=["GET"])
+def index():
+    return 'Digite /returnstuff/elemento/seletor_css/page'
 
 @app.route('/returnstuff/<string:elemento>/<string:seletor_css>/<path:page>', methods=["GET"])
 def returnstuff(elemento, seletor_css, page):
